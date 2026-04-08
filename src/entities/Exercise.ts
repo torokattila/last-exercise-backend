cdd1f29769da2786a0523796f7a6f3ac87787981
Török Attila <torcsiattila93@gmail.com>
2026-04-08 19:24:36 +0200

    feat: intervalNotificationTime property added to exercise

--- src/entities/Exercise.ts
+++ src/entities/Exercise.ts
@@ -32,4 +32,7 @@ export default class Exercise extends EntityBase {
 
   @Column({ name: 'order', default: 1 })
   order: number;
+
+  @Column({ name: 'interval_notification_time', nullable: true })
+  intervalNotificationTime: string;
 }
